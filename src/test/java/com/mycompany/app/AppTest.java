39a4ae3f36510ec26095a5a50352a036f5fc7b7f
Sveta-ls <slana6570@gmail.com>
2026-04-29 00:25:58 +0300

    actions

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -8,7 +8,7 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 
 public class AppTest {
-    private Game game;
+    private Game game; \\actions
 
     @BeforeEach
     void setUp() {
